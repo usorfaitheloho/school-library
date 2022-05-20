d0b3e0847ce737b2ad2e52b3a1d1f620fed194d9
usorfaitheloho <usorfaitheloho@gmail.com>
2022-05-20 15:08:48 -0700

    Refactor code

--- rental.rb
+++ rental.rb
@@ -2,17 +2,11 @@ class Rental
     attr_accessor :date
     attr_reader :book, :person
 
-    def initialize(date,book,person)
+    def initialize(date, book, person)
         super()
         @date = date
-    end
-
-    def book=(book)
         @book = book
         book.rental.push(self) unless book.rental.includes?(self)
-    end
-
-    def person=(person)
         @person = person
         person.rental.push(self) unless person.rental.includes?(self)
       end
