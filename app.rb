f552fc591376457ae478f53ce4ee17361371060a
usorfaitheloho <usorfaitheloho@gmail.com>
2022-05-21 15:12:14 -0700

    initialize app

--- app.rb
+++ app.rb
@@ -4,3 +4,21 @@ require './person'
 require './rental'
 require './teacher'
 require './student'
+
+class App
+    attr_accessor :input
+
+    def initialize
+        puts 'welcome to the school library App!'
+        prompt
+        @rental = []
+        @people = []
+        @classroom_default = classroom.new('default_classroom')
+        @books = []
+        @input = gets.chomp
+    end
+
+    def prompt
+        puts
+    end
+end
